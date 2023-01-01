4c6ade93e3239c904861499bdd0b4d760a3d1480
abhishekjoshigamerz <abhishekjoshicctian1@gmail.com>
2023-01-01 21:49:20 +0530

    Commit basic app finished 1. add favorite, remove favorite 2. view details almost finished

--- js/viewDetails.js
+++ js/viewDetails.js
@@ -17,16 +17,50 @@ const getData = async function (id) {
 };
 
 function renderResultWithDetails(data) {
+  let ingredients = {};
+
+  for (let i = 1; i < 21; i++) {
+    // let dd = `strIngrednts${i}`;
+    // console.log(data.dd);
+    const key = `strIngredient${i}`;
+    const key2 = `strMeasure${i}`;
+    const value = data[key];
+    const valueMeasure = data[key2];
+    if (value) {
+      ingredients[value] = valueMeasure;
+    }
+  }
+  console.log(ingredients);
+  let html = "";
+  for (const key in ingredients) {
+    html += `<p>${key} : ${ingredients[key]}</p>`;
+  }
+
+  let button = `<button class="btn-4" data-id="${data.idMeal}">Add to Favorite</button>`;
+  if (localStorage.getItem(data.idMeal)) {
+    button = `<button class="btn-4" data-id="${data.idMeal}">Remove From Favorites</button>`;
+  }
+
   markUp += `<div class="result--Details">
     <div class="detail--intro">
         <div class="detail--img"><img src="${data.strMealThumb}" /></div>
-        <div class="detail--desc text-center"><h2 >${data.strMeal}</h2>
-        <h4>Tags: ${data.strTags}</h4>
-        <h4>Category: ${data.strCategory}</h4>
-        <h4>Area: ${data.strArea}</h4>
+        <div class="detail--desc text-center">
+            <h2 >${data.strMeal}</h2>
+            <h4>Tags: ${data.strTags}</h4>
+            <h4>Category: ${data.strCategory}</h4>
+            <h4>Area: ${data.strArea}</h4>
+            <h4>Ingredients</h4>
+            <p class="ml-2 mr-2">${html}</p> 
+            <br />
+            <h5><a href="${data.strSource}" target='_blank' ">Original Source</a> </h5>
+            ${button}    
+            
         </div>
     </div>
-    
+    <div class="detail--cooking">
+    <h2 class="text-center">How to Cook</h2>
+    ${data.strInstructions}
+    </div>
   </div>`;
 
   displaySection.insertAdjacentHTML("beforeend", markUp);
